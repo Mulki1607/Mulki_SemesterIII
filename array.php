c81256197d302e16bc53cad6ac8a11c24015450b
unknown <malikul1219@gmail.com>
2025-09-08 11:38:25 +0700

    upload php array, dan beberapa pembelajaran lain yang ada di folder

---
+++ array.php
@@ -0,0 +1,9 @@
+<?php 
+
+$myarr = array("Handphone", 1000, "sabtu");
+echo " Saya membeli $myarr[0] dengan harga $myarr[1] pada hari sabtu<br>";
+foreach($myarr as $initial)
+{
+    echo "Isi array: $initial <br>";
+}
+?>
